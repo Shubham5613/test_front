06518fc09ae5cd7117bf8c2282c8eb84b6b9f116
Shubham Singh <k.shubham@Shubhams-MacBook-Air.local>
2025-07-26 01:02:45 +0530

    all changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-// import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "@/pages/Index";
-import CampaignForm from "@/components/campaignForm";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import CampaignForm from "./components/CampaignForm";
 
 const queryClient = new QueryClient();
 
-const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/campaign-info" element={<CampaignForm />} />
-            <Route path="/ad-entries" element={<CampaignForm />} />
-            <Route path="/results" element={<CampaignForm />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-};
-;
-
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/campaign-info" element={<CampaignForm />} />
+          <Route path="/ad-entries" element={<CampaignForm />} />
+          <Route path="/results" element={<CampaignForm />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
